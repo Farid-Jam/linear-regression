5cea0d4d131c83a7acbd1b8e33f45f20128f89c2
Farid-Jam <farid.jamshid77@gmail.com>
2025-05-15 12:39:20 -0400

    Cleaned and tested with updated data to determine how accurate system can get

--- multivariateLinearRegression.py
+++ multivariateLinearRegression.py
@@ -3,18 +3,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("data/multivariate_data.csv")
-data = data[['age', 'studytime', 'absences', 'G1', 'G2', 'G3']]
+data = data.replace({'yes': 1, 'no': 0})
+data['sex'] = data['sex'].map({'M': 1, 'F': 0})
+data['famsize'] = data['famsize'].map({'LE3': 1, 'GT3': 0})
+data['school'] = data['school'].map({'GP': 1, 'MS': 0})
+data['address'] = data['address'].map({'U': 1, 'R': 0})
+data['Pstatus'] = data['Pstatus'].map({'A': 1, 'T': 0})
+data['Mjob'] = data['Mjob'].map({'at_home': 0, 'health': 1, 'other': 2, 'services': 3, 'teacher': 4})
+data['Fjob'] = data['Fjob'].map({'at_home': 0, 'health': 1, 'other': 2, 'services': 3, 'teacher': 4})
+data['reason'] = data['reason'].map({'course': 0, 'home': 1, 'reputation': 2, 'other': 3})
+data['guardian'] = data['guardian'].map({'mother': 0, 'father': 1, 'other': 2})
+print(data)
 
 y = data[['G3']].to_numpy()
-x = data[['age', 'studytime', 'absences', 'G1', 'G2']].to_numpy()
+x = data.drop(columns=['G3']).to_numpy()
 x = np.hstack((np.ones((x.shape[0], 1)), x))
-x_transpose = np.transpose(x)
-x_t_times_x = np.matmul(x_transpose, x)
-x_inverse = np.linalg.inv(x_t_times_x)
-b = np.matmul(x_inverse, x_transpose)
-b = np.matmul(b, y)
 
-y_pred = np.matmul(x, b)
+b = np.linalg.inv(x.T @ x) @ x.T @ y
+
+y_pred = x @ b
 
 residuals = y - y_pred
 
@@ -25,3 +32,4 @@ mse = np.mean(residuals**2)
 result = np.hstack((y, y_pred))
 
 print(result)
+print(mse)
